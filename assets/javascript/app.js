219f5b583d6bb96191705873fc3d6c4e455c0dcf
Tommy Vinyard <tpvinyard@gmail.com>
2019-08-25 22:31:40 -0500

    adding start/stop on the click of the gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,74 +1,50 @@
-const topics = ['corgi', 'wolf'];
+$(document).ready(function() {
 
-for (let i=0; i < topics.length; i++) {
-    $("#buttons").append(`<button data-dog='${topics[i]}'>${topics[i]}</button>`);
-}
+    const topics = ['corgi', 'wolf'];
 
-$("button").on("click", function() {
-    let dog = $(this).attr("data-dog");
-    queryURL = "https://api.giphy.com/v1/gifs/search?api_key=CirucykHGzeIGsP6elqaIVUsqTllQ1OG&q=" + dog + "&limit=10";
+    for (let i=0; i < topics.length; i++) {
+        $("#buttons").append(`<button data-dog='${topics[i]}'>${topics[i]}</button>`);
+    }
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        let results = response.data;
-        for (let i = 0; i < results.length; i++) {
-            let gifDiv = $("<div>");
-            let rating = results[i].rating;
-            let h4 = $("<h4>").text(rating);
-            let dogImage = $("<img>");
-            dogImage.attr("src", results[i].images.fixed_height.url);
+    $("button").on("click", function() {
+        let dog = $(this).attr("data-dog");
+        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=CirucykHGzeIGsP6elqaIVUsqTllQ1OG&q=" + dog + "&limit=10";
 
-            gifDiv.append(h4);
-            gifDiv.append(dogImage);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            let results = response.data;
+            for (let i = 0; i < results.length; i++) {
+                let gifDiv = $("<div>");
+                let rating = results[i].rating;
+                let h4 = $("<h4>").text(rating);
+                let dogImage = $("<img>");
+                dogImage.attr("src", results[i].images.fixed_height_still.url);
+                dogImage.attr("class", "gif");
+                dogImage.attr("data-still", results[i].images.fixed_height_still.url);
+                dogImage.attr("data-animate", results[i].images.fixed_height.url);
+                dogImage.attr("data-state", "still");
 
-            $("#images").append(gifDiv);
+                gifDiv.append(h4);
+                gifDiv.append(dogImage);
+
+                $("#images").append(gifDiv);
+            }
+        })
+    })
+
+    $(document).on("click",".gif", function() {
+
+        let state = $(this).attr("data-state");
+
+        if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+        } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
         }
     })
-})
 
-// $("button").on("click", function() {
-//     let person = $(this).attr("data-person");
-//     queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//       person + "&api_key=dc6zaTOxFJmzC&limit=10";
-// ​
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     })
-//       .then(function(response) {
-//         var results = response.data;
-// ​
-//         for (var i = 0; i < results.length; i++) {
-//           var gifDiv = $("<div>");
-// ​
-//           var rating = results[i].rating;
-// ​
-//           var p = $("<p>").text("Rating: " + rating);
-// ​
-//           var personImage = $("<img>");
-//           personImage.attr("src", results[i].images.fixed_height.url);
-// ​
-//           gifDiv.prepend(p);
-//           gifDiv.prepend(personImage);
-// ​
-//           $("#gifs-appear-here").prepend(gifDiv);
-//         }
-//       });
-//   });
-
-// $("#cat-button").on("click", function() {
-//   let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=CirucykHGzeIGsP6elqaIVUsqTllQ1OG&tag=cats";
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-//     .then(function(response) {
-//       var imageUrl = response.data.image_original_url;
-//       var catImage = $("<img>");
-//       catImage.attr("src", imageUrl);
-//       catImage.attr("alt", "cat image");
-//       $("#images").prepend(catImage);
-//     });
-// });
+});
