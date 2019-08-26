0c1ff8e54e58951854bf1320fd4937e5582787d3
Tommy Vinyard <tpvinyard@gmail.com>
2019-08-25 21:41:15 -0500

    10 gifs append to the window based on the text/attributes of the button clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,32 @@
 const topics = ['corgi', 'wolf'];
 
 for (let i=0; i < topics.length; i++) {
-    $("#buttons").append(`<button>${topics[i]}</button>`);
+    $("#buttons").append(`<button data-dog='${topics[i]}'>${topics[i]}</button>`);
 }
 
-// $("#cat-button").on("click", function() {
-//   let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=CirucykHGzeIGsP6elqaIVUsqTllQ1OG&tag=cats";
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-//     .then(function(response) {
-//       var imageUrl = response.data.image_original_url;
-//       var catImage = $("<img>");
-//       catImage.attr("src", imageUrl);
-//       catImage.attr("alt", "cat image");
-//       $("#images").prepend(catImage);
-//     });
-// });
+$("button").on("click", function() {
+    let dog = $(this).attr("data-dog");
+    queryURL = "https://api.giphy.com/v1/gifs/search?api_key=CirucykHGzeIGsP6elqaIVUsqTllQ1OG&q=" + dog + "&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        let results = response.data;
+        for (let i = 0; i < results.length; i++) {
+            let gifDiv = $("<div>");
+            let rating = results[i].rating;
+            let h4 = $("<h4>").text(rating);
+            let dogImage = $("<img>");
+            dogImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.append(h4);
+            gifDiv.append(dogImage);
+
+            $("#images").append(gifDiv);
+        }
+    })
+})
 
 // $("button").on("click", function() {
 //     let person = $(this).attr("data-person");
@@ -48,3 +57,18 @@ for (let i=0; i < topics.length; i++) {
 //         }
 //       });
 //   });
+
+// $("#cat-button").on("click", function() {
+//   let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=CirucykHGzeIGsP6elqaIVUsqTllQ1OG&tag=cats";
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//     .then(function(response) {
+//       var imageUrl = response.data.image_original_url;
+//       var catImage = $("<img>");
+//       catImage.attr("src", imageUrl);
+//       catImage.attr("alt", "cat image");
+//       $("#images").prepend(catImage);
+//     });
+// });
